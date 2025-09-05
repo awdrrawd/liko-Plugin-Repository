07a5b704b59e482b48a01b0633a2c5a5e84dc5a7
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-09-05 10:27:03 +0800

    Create Liko-Bondage_renew.user.js

---
+++ Plugins/Liko-Bondage_renew.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Liko - Bondage renew
+// @name:zh      Liko的綑綁刷新
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  Bondage Club - Likolisu's Bondage renew
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20Bondage%20renew.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[CHE] loadtime:', Date.now());
+})();
