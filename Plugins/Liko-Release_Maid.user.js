16634f341083f3ce85a71e7423305368a1a811fb
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-09-05 10:50:13 +0800

    Update Liko-Release_Maid.user.js

--- Plugins/Liko-Release_Maid.user.js
+++ Plugins/Liko-Release_Maid.user.js
@@ -15,7 +15,7 @@
 (function () {
     "use strict";
     const script = document.createElement("script");
-    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20Release%20Maid.user.js?timestamp=${Date.now()}`;
+    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20Release%20Maid.main.user.js?timestamp=${Date.now()}`;
     script.type = "module";
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
