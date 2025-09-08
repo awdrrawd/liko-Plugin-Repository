e90653a355adfeadd3f1d5ed3c33e4c8ac70736c
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-09-09 00:33:02 +0800

    Update Liko - Plugin Collection Manager.main.user.js

--- Plugins/main/Liko - Plugin Collection Manager.main.user.js
+++ Plugins/main/Liko - Plugin Collection Manager.main.user.js
@@ -8,6 +8,7 @@
 // @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
 // @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
 // @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/BC_toast_system.user.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
