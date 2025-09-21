9c4de3125528ab783f57a4e5a5a1f05adecb4c42
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-09-21 14:38:41 +0800

    Create Liko-CDB.user.js

---
+++ Plugins/Liko-CDB.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Liko - CDB
+// @name:zh      Liko的自訂更衣室背景
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  自訂更衣室背景 | Custom Dressing Background
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20CDB.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[CDT] loadtime:', Date.now());
+})();
