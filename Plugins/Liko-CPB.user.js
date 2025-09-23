b11d1320845a5f53d6d06e829a24ff492edb520a
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-09-23 23:55:26 +0800

    Create Liko-CPB.user.js

---
+++ Plugins/Liko-CPB.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Liko - Custom Profile Background
+// @name:zh      Liko的自定義個人資料頁面背景
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  自定義個人資料頁面背景
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20CPB.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[CPB] loadtime:', Date.now());
+})();
