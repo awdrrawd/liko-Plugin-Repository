85f7b55b75f815520cb7913ee93878f0ecc6de30
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2025-10-06 17:01:56 +0800

    Create Liko-CMC.user.js

---
+++ Plugins/Liko-CMC.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Liko - Chat Music Controller
+// @name:zh      Liko的聊天室音樂控制器
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  Chat Music Controller with lyrics support
+// @author       莉柯莉絲(Likolisu)
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/main/Liko%20-%20CMC.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[CMC] loadtime:', Date.now());
+})();
