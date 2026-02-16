3d41755383953c5ddd6659a3f86e366d958630fa
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-02-16 16:54:17 +0800

    Create Liko-WPS.user.js

---
+++ Plugins/Liko-WPS.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Liko - WPS
+// @name:zh      WCE的Profile分享
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  WCE的Profile分享 | WCE Profile Share
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/main/Liko%20-%20WPS.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[WPS] loadtime:', Date.now());
+})();
