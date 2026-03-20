e8f6dfe7ab9609ca51504e25472241b4d01232e0
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-03-20 12:43:48 +0800

    Create Liko-MAT.user.js

---
+++ Plugins/Liko-MAT.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Liko - MAT
+// @name:zh      Liko的自動翻譯(使用Google api)
+// @namespace    https://likolisu.dev/
+// @version      1.0
+// @description  Automatically translate BC chat messages using Google API.
+// @author       Liko
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/main/Liko%20-%20MAT.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[MAT] loadtime:', Date.now());
+})();
