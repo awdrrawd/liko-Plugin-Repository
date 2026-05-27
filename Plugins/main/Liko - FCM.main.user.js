67aa2876431e1b92eb6382335e84f406909565d1
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-05-28 01:15:15 +0800

    Update Liko - FCM.main.user.js

--- Plugins/main/Liko - FCM.main.user.js
+++ Plugins/main/Liko - FCM.main.user.js
@@ -6,6 +6,7 @@
 // @description  Friends and ChatRoom Manager | 好友與房間管理
 // @author       Likolisu
 // @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
 // @grant        none
 // @require      https://cdn.jsdelivr.net/gh/Jomshir98/bondage-club-mod-sdk@0.3.3/dist/bcmodsdk.js
 // @run-at       document-end
