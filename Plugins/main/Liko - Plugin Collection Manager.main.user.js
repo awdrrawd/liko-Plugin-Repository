d66dac6641ab27729bc5e5825252874277b2588a
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-05-31 09:22:56 +0800

    Update Liko - Plugin Collection Manager.main.user.js

--- Plugins/main/Liko - Plugin Collection Manager.main.user.js
+++ Plugins/main/Liko - Plugin Collection Manager.main.user.js
@@ -1999,7 +1999,7 @@
     try {
         if (bcModSdk?.registerMod) {
             modApi = bcModSdk.registerMod({
-                name: "Liko's PCM",
+                name: "Liko - PCM",
                 fullName: 'Liko - Plugin Collection Manager',
                 version: modversion,
                 repository: 'Liko的插件管理器 | Plugin collection manager',
