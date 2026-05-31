f6bf539a7408423e2862d17fcb62c4f914856e69
awdrrawd <58585499+awdrrawd@users.noreply.github.com>
2026-05-31 09:18:53 +0800

    Update Liko - Release Maid.main.user.js

--- Plugins/main/Liko - Release Maid.main.user.js
+++ Plugins/main/Liko - Release Maid.main.user.js
@@ -50,7 +50,7 @@
 
         try {
             modApi = bcModSdk.registerMod({
-                name: 'Liko Release Maid',
+                name: 'Liko - Release Maid',
                 fullName: 'Bondage Club - Liko Auto Release Maid',
                 version: '1.2',
                 repository: '莉柯莉絲的自動解鎖女僕',
