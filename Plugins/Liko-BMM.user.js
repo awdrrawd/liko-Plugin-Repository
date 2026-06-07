03062f0c86b1fc5136fa560207ce76ca76978a30
Likolisu <58585499+awdrrawd@users.noreply.github.com>
2026-06-07 12:20:11 +0800

    Create Liko-BMM.user.js

---
+++ Plugins/Liko-BMM.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         liko - BMM
+// @namespace    https://github.com/awdrrawd/liko-Plugin-Repository
+// @version      1.0
+// @description  BC 地圖房迷你地圖
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @run-at       document-end
+// @require      https://cdn.jsdelivr.net/gh/awdrrawd/liko-Plugin-Repository@main/Plugins/expand/bcmodsdk.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/main/Liko%20-%20BMM.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[BMM] loadtime:', Date.now());
+})();
