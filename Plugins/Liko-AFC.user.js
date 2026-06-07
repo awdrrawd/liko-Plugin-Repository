008dafae85c7601536a343ac538207a7c977f083
Likolisu <58585499+awdrrawd@users.noreply.github.com>
2026-06-07 11:53:03 +0800

    Create Liko-AFC.user.js

---
+++ Plugins/Liko-AFC.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         BC Abundantia Florum ─Chromatica─
+// @name:zh      BC 繁戀如花 ─繽紛─
+// @namespace    https://github.com/awdrrawd/liko-Plugin-Repository
+// @version      1.0
+// @description  拓展戀人系統 | Extended Lover System for BondageClub
+// @author       Likolisu
+// @include      /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon         https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @grant        none
+// @require      https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/expand/bcmodsdk.js
+// @run-at       document-end
+// ==/UserScript==
+https://github.com/awdrrawd/BC-AFC/raw/refs/heads/main/Plugins/Liko-AFC.user.js
+(function () {
+    "use strict";
+    const url = `https://raw.githubusercontent.com/awdrrawd/BC-AFC/main/Plugins/Liko-AFC.user.js?timestamp=${Date.now()}`;
+    fetch(url)
+        .then(r => r.text())
+        .then(code => {
+            const fn = new Function(code);
+            fn();
+        })
+        .catch(e => console.error('[ACF] 載入失敗:', e));
+})();
