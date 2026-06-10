2b8a619317f1f45c1b5318f6adfbc2c3056c33cf
Likolisu <58585499+awdrrawd@users.noreply.github.com>
2026-06-10 13:30:46 +0800

    Create Liko-MPL.user.js

---
+++ Plugins/Liko-MPL.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name           Liko - Mobile Portrait Layout
+// @name:zh        Liko的手機直版佈局
+// @namespace      https://github.com/awdrrawd/liko-Plugin-Repository
+// @version        0.2.0
+// @description    Supports vertical layout for ChatSearch and ChatRoom
+// @description:zh 支援房間搜尋與聊天室的直版佈局
+// @include        /^https:\/\/(www\.)?bondage(projects\.elementfx|-(europe|asia))\.com\/.*/
+// @icon           https://raw.githubusercontent.com/awdrrawd/liko-tool-Image-storage/refs/heads/main/Images/LOGO_2.png
+// @author         Likolisu
+// @grant          none
+// @require        https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/expand/bcmodsdk.js
+// @run-at         document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const script = document.createElement("script");
+    script.src = `https://awdrrawd.github.io/liko-Plugin-Repository/Plugins/main/Liko%20-%20MPL.main.user.js?timestamp=${Date.now()}`;
+    script.type = "module";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log('[BMM] loadtime:', Date.now());
+})();
