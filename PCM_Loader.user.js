7fffd0a2a8115dfcfe67cd95f15f9081530003b8
Likulisu <awdrrawd1@gmail.com>
2026-08-25 19:07:11 +0800

    Update PCM_Loader.user.js

--- PCM_Loader.user.js
+++ PCM_Loader.user.js
@@ -2,7 +2,7 @@
 // @name           Liko - Plugin Collection Manager-Loader
 // @name:zh        Liko的插件管理器-Loader
 // @namespace      https://github.com/awdrrawd/liko-Plugin-Repository
-// @version        1.2.1
+// @version        1.2.2
 // @description    Liko's Plugin Collection Manager
 // @author         Likolisu
 // @include      /^https:\/\/(www\.)?(bondage(projects\.elementfx|-(europe|asia))\.com|bondageeurope\.com)\/R*/
